d9e69bdbff8402aa710afe10834fc7d6008b8f53
Ray Brennan <ray@quitealright.com>
2026-03-14 14:33:00 +0000

    Updated Window Title. Renamed MainPage (Clean code).

--- UserManagement.Maui/App.xaml.cs
+++ UserManagement.Maui/App.xaml.cs
@@ -11,6 +11,7 @@
         {
             Window window = new Window(new AppShell());
             window.Width = 400;
+            window.Title = "Democracy Counts";
             return window;
         }
     }
